2dbdd79edc07f1b08f09cc68ffa62b2f6b863865
veedee <43088920+veedee2000@users.noreply.github.com>
2019-09-05 02:27:17 +0530

    Add files via upload

---
+++ c1w5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    Varun Das :)
+  </title>
+</head>
+<body>
+  <h1>Varun Das :)</h1>
+  <?php
+    echo "The ouput of my name is :";
+    print hash('sha256', 'Varun Das');
+  ?>
+  <pre>
+        some
+            text
+                with
+                    pre
+                        tag
+  </pre>
+  <a href= "http://localhost:8888/c1w5/check.php" target = "_blank"> The check.php code </a>
+  <br>
+  <a href= "http://localhost:8888/c1w5/fail.php" target = "_blank"> The fail.php code </a>
+
+</body>
+
+</html>
